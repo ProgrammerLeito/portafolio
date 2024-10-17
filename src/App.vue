154835b9c402eef242a843ade2aa9e6d1d7a13b8
Leonardo <gleo62594@gmail.com>
2024-10-17 17:16:04 -0500

    avance en inicio

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+const abrirMenuHamburguer = ref(false);
+
+</script>
+
+<template>
+  <header class="bg-gray-800 border-b-2 border-gray-900 sticky top-0 hidden">
+      <nav class="flex flex-row justify-between">
+        <div class="flex flex-row justify-between items-center w-[90%] mx-auto">
+          <div>
+              <img class="w-16 cursor-pointer" src="/public/Img/logo2.png" alt="...">
+          </div>
+          <div class="bg-red-600">
+          </div>
+          <div class="w-full">
+            <div class="text-white duration-500 bg-gray-800 md:min-h-[8vh] left-0 md:w-auto w-full flex justify-end items-center">
+              <ul class="sm:flex md:flex-row md:items-center hidden justify-end md:gap-[4vw] gap-8">
+                  <li>
+                      <a class="hover:text-gray-500" href="#">Inicio</a>
+                  </li>
+                  <li>
+                      <a class="hover:text-gray-500" href="#">Solution</a>
+                  </li>
+                  <li>
+                      <a class="hover:text-gray-500" href="#">Resource</a>
+                  </li>
+                  <li>
+                      <a class="hover:text-gray-500" href="#">Contacto</a>
+                  </li>
+              </ul>
+            </div>
+          </div>
+          <div class="flex items-center gap-6 text-white">
+              <ion-icon @click="abrirMenuHamburguer" name="menu" class="text-3xl cursor-pointer sm:hidden"></ion-icon>
+          </div>
+        </div>
+      </nav>
+  </header>
+  <div id="inicio">
+    <HelloWorld />
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import Typewriter from 'typewriter-effect/dist/core'
+
+const typewriter = ref(null)
+
+onMounted(() => {
+  new Typewriter(typewriter.value, {
+    loop: true,    // Para que el efecto se repita
+    delay: 75,     // Velocidad al escribir las letras
+    deleteSpeed: 60 // Velocidad al borrar las letras
+  })
+    .typeString('Desarrollador Frontend')  // Escribe el texto
+    .pauseFor(1500)  // Pausa antes de borrar
+    .deleteAll()     // Borra todo el texto
+    .pauseFor(1000)  // Pausa antes de volver a escribir
+    .start()         // Inicia el bucle
+})
+</script>
+
+<template>
+  <div class="w-full flex flex-row justify-center items-center min-h-[calc(100vh-64px)] relative">
+    <img class="w-full h-screen object-cover" src="/public/Img/fondo.jpg" alt="Logo"/>
+
+    <div class="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-5"></div>
+
+    <div class="absolute inset-0 flex flex-col justify-center items-center text-white z-10 gap-y-2.5" data-aos="zoom-in-up">
+      <h1 class="text-4xl font-bold">LEONARDO GARCIA JIMENEZ</h1>
+      <div class="text-xl" ref="typewriter"></div>
+      <h2 class="text-sm whitespace-break-spaces px-80">
+        Soy un desarrollador especializado en la creación de soluciones tecnológicas para la optimización de procesos administrativos. Mi objetivo es proporcionar sistemas personalizados y escalables que respondan a las necesidades específicas de cada negocio, impulsando la transformación digital y facilitando la gestión integral de las organizaciones.
+      </h2>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.text-xl {
+  font-size: 1.25rem; /* O ajusta según tus preferencias */
+}
+</style>
